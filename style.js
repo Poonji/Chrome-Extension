bec4f66cf6200ba2a9b19130b9d5b0f3139dc3f2
Poonji <poonjij21@gmail.com>
2025-03-02 02:38:37 +0530

    done

--- style.js
+++ style.js
@@ -1,32 +1,48 @@
-let myLeads=[]
-const inputEl=document.getElementById("input-el")
-const inputBtn=document.getElementById("input-btn")
-const ulEl=document.getElementById("ul-el")
- inputBtn.addEventListener("click", function()
-{
-    myLeads.push(inputEl.value)
-    inputEl.value="" // clearing the i/p feild after displaying
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))//  stores data in the browser's localStorage
-    renderLeads()
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/poonji-jadon/"}
+]
+
+tabBtn.addEventListener("click", function(){
+    // Save the url instead of logging it out
+    console.log(tabs[0].url)
 })
-function renderLeads(){
-    let ListItems=""
-for(let i=0;i<myLeads.length;i++)
-{ 
-    ListItems+=`
-    <li>
-    <a target='_blank' href='${myLeads[i]}'>
-    ${myLeads[i]}
-    </a>
-    </li>
-    ` 
-}
-    ulEl.innerHTML=ListItems //adding HTML from JS
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
 
-/*  const recipient = "James"
-let sender=Poonji
-// Refactor the email string to use template strings
-const email = `Hey ${recipient}!(template string) How is it going? Cheers ${sender}`
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-console.log(email) */
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
